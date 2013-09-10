37f69b4f3f033f5a46cb8998f1c1c4dd29936e05
axgort <jastrzab5@gmail.com>
2013-09-10 23:25:58 +0200

    Poprawka do danych bo byly zjebane

--- data/Curriculum_based_Course_timetabling/datasets/get.sh
+++ data/Curriculum_based_Course_timetabling/datasets/get.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 for x in `seq 1 21`; do
-    wget http://www.cs.qub.ac.uk/itc2007/curriculmcourse/initialdatasets/comp0$x.ctt
+    if [[ $x -lt 10 ]]; then
+        wget http://www.cs.qub.ac.uk/itc2007/curriculmcourse/initialdatasets/comp0$x.ctt
+    else
+        wget http://www.cs.qub.ac.uk/itc2007/curriculmcourse/initialdatasets/comp$x.ctt
+    fi
 done
