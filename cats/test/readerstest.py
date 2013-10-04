f313b6d18fff300e2d2d6e8673caeaf13432bf64
tomecki <tomek.dziopa@gmail.com>
2013-10-04 22:45:04 +0200

    Biblioteka do odczytu danych v1

--- cats/test/readerstest.py
+++ cats/test/readerstest.py
@@ -1,13 +1,15 @@
 import unittest
-
+from cats.readers.competition import CompetitionReader
 
 class ReadTest(unittest.TestCase):
     def test_read(self):
-        print "HURRA!"
-        self.assertTrue(1==1, "dziala!")
+        c = CompetitionReader(1)
+        self.assertEquals(c.getInstanceName(), "Fis0506-1")
+        self.assertEquals(c.getConstraintNum(), len(c.constraints))
+        self.assertEquals(c.getCoursesNum(), len(c.courses))
+        self.assertEquals(c.getCurriculumNum(), len(c.curricula))
+        self.assertEquals(c.getRoomsNum(), len(c.rooms))
 
-    def test_2(self):
-        self.assertTrue(2==1)
 
 if __name__=="__main__":
     unittest.main()
