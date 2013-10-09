8e49b7ee6bb50a07a0bf75ee1e17e4ac8cd7c384
axgort <jastrzab5@gmail.com>
2013-10-09 09:24:50 +0200

    Male czyszczenie

--- cats/utils/data.py
+++ cats/utils/data.py
@@ -10,23 +10,6 @@ class Data(object):
         self.daysNum = 0
         self.periodsPerDayNum = 0
 
-    def getCoursesNum(self):
-        """Return number of courses"""
-        return len(self.courses)
-
-    def getRoomsNum(self):
-        """Return number of rooms"""
-        return len(self.rooms)
-
-    def getCurriculumNum(self):
-        """Return number of curriculum"""
-        return len(self.curricula)
-
-    def getConstraintNum(self):
-        """Return number of constraints"""
-        return len(self.constraints)
-
-
 
 
 
