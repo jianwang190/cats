f956d24443452216517942a595111f8ea97a6f32
KasiaSmietanka <kasia.smietanka@gmail.com>
2013-10-12 16:21:01 +0200

    Implementacja funkcji: the total number of available periods for courseId

--- cats/tabuSearch/tabuSearch.py
+++ cats/tabuSearch/tabuSearch.py
@@ -1,12 +1,17 @@
 import itertools
 from cats.utils.inputDataStructures import Course, Room, Constraint, Curriculum
+from cats.utils.data import Data
+
+class CellOfTimeTable(object):
+    def __init__(self, courseId = [], roomId = []):
+        self.courseId = courseId
+        self.roomId = roomId
 
 class TimeTable(object):
     def __init__(self, daysNum, periodsPerDayNum):
 	    # courseId roomId
     	self.periodsPerDayNum = periodsPerDayNum
         self.daysNum = daysNum
-        self.slot = []
         self.timeSlots = range(self.daysNum * self.periodsPerDayNum)
         self.timeTable = {x : [] for x in self.timeSlots}
         self.neighbourhoodList = {}
@@ -42,10 +47,17 @@ class TimeTable(object):
         keysConstraintsOfCourse = map(self.mapKeys, [ x for x in constraintsList if x.id == courseId])
         return keysConstraintsOfCourse
 
-    """Count number of available slots for course TODO : consider neighourhood"""
+    """Check if slot if available for course, considering neighbourhood (conflicts courses)"""
+    def checkIfAvailable(self, timeTableList, courseId):
+        for cell in timeTableList:
+            if (cell is not []) and ((cell.courseId in self.neighbourhoodList[courseId]) or (cell.courseId == courseId)):
+                return False
+        return True
+
+    """Count number of available slots for course, function considers neighourhood"""
     def availableNumberOfPeriods(self, constraintsList, courseId):
         keysConstraintsOfCourse = set(self.getKeyConstraintsOfCourse(constraintsList, courseId))
-        availableSlots = set(filter(lambda x: self.timeTable[x] == self.slot, self.timeTable))
+        availableSlots = set (filter(lambda x: self.checkIfAvailable(self.timeTable[x], courseId) == True, self.timeTable))
         counter = len(availableSlots - keysConstraintsOfCourse)
         return counter
 
--- cats/test/tabuSearchTest.py
+++ cats/test/tabuSearchTest.py
@@ -1,6 +1,6 @@
 import unittest
 import os
-from cats.tabuSearch.tabuSearch import TimeTable
+from cats.tabuSearch.tabuSearch import TimeTable, CellOfTimeTable
 from cats.readers.competitionReader import CompetitionReader
 from random import randint
 from cats.utils.data import Data
@@ -10,12 +10,12 @@ class TabuSearchTest(unittest.TestCase):
         self.c = CompetitionReader()
         self.data = self.c.readInstance(1)
         self.t = TimeTable(self.data.daysNum, self.data.periods_per_day)
-
+	self.t.neighbourhoodList = self.t.createNeighbourhoodList(self.data.curricula, self.data.courses)
     def test_initial(self):
         self.assertEquals(len(self.t.getTimeTable()), 30)
         day = randint(0, self.data.daysNum - 1)
         day_period = randint(0, self.data.periods_per_day - 1)
-        self.assertEquals(self.t.getValueSlot(day, day_period), [])
+    	self.assertEquals(self.t.getValueSlot(day, day_period), [])
 
     def test_getKeyConstraintsOfCourse(self):
         keysConstraintsOfCourse = self.t.getKeyConstraintsOfCourse(self.data.constraints, 'c0001')
@@ -28,14 +28,30 @@ class TabuSearchTest(unittest.TestCase):
         self.assertEqual(counter, 30)
 
     def test_availableNumberOfPeriods2(self):
-        self.t.timeTable[0] = ['c0001', 'B']
-        self.t.timeTable[1] = ['c0002', 'B']
-        self.t.timeTable[2] = ['c0001', 'B']
-        self.t.timeTable[24] = ['c0001', 'B']
-        counter = self.t.availableNumberOfPeriods(self.data.constraints, 'c0002')
-        self.assertEqual(counter, 26)
+        self.t.timeTable[0].append(CellOfTimeTable('c0001', 'B'))
+        self.t.timeTable[1].append(CellOfTimeTable('c0002', 'B'))
+        self.t.timeTable[2].append(CellOfTimeTable('c0001', 'B'))
+        self.t.timeTable[24].append(CellOfTimeTable('c0001', 'B'))
         counter = self.t.availableNumberOfPeriods(self.data.constraints, 'c0001')
         self.assertEqual(counter, 21)
+        counter = self.t.availableNumberOfPeriods(self.data.constraints, 'c0002')
+        self.assertEqual(counter, 26)
+
+    def test_availableNumberOfPeriods3(self):
+        self.t.timeTable[0].append(CellOfTimeTable('c0001', 'B'))
+        self.t.timeTable[1].append(CellOfTimeTable('c0002', 'B'))
+        self.t.timeTable[1].append(CellOfTimeTable('c0014', 'C'))
+        self.t.timeTable[3].append(CellOfTimeTable('c0014', 'C'))
+        self.t.timeTable[2].append(CellOfTimeTable('c0004', 'C'))
+        self.t.timeTable[24].append(CellOfTimeTable('c0001', 'B'))
+        counter = self.t.availableNumberOfPeriods(self.data.constraints, 'c0001')
+        self.assertEqual(counter, 21)
+        counter = self.t.availableNumberOfPeriods(self.data.constraints, 'c0002')
+        self.assertEqual(counter, 26)
+        counter = self.t.availableNumberOfPeriods(self.data.constraints, 'c0014')
+        self.assertEqual(counter, 28)
+        counter = self.t.availableNumberOfPeriods(self.data.constraints, 'c0071')
+        self.assertEqual(counter, 15)
 
     def test_createNeighbourhoodList(self):
         neighbourhoodList = self.t.createNeighbourhoodList(self.data.curricula, self.data.courses)
