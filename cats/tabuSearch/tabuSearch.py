201da2240cc0610eb389453d11e327f758dca000
KasiaSmietanka <kasia.smietanka@gmail.com>
2013-10-31 09:56:29 +0100

    Functions to implement

---
+++ cats/tabuSearch/tabuSearch.py
@@ -0,0 +1,33 @@
+"""Tabu search algorithm"""
+"""Period related costs - sum(minimumWorkingDays, curriculumCompactness)"""
+"""Room related costs - sum(roomCapacity, roomStability)"""
+
+PERIOD_RELATED_COST_TAU = 2
+DEPTH_OF_TABU_SEARCH = 10
+
+def simpleSwap(timetable):
+    pass
+
+"""Single and double kempe swap"""
+def kempeSwap(timetable):
+    pass
+
+"""Matching algorithm to make room allocations"""
+def matchingRoomAllocations(timetable):
+    pass
+
+"""page 9 description"""
+def tabuTenure(courseId):
+    pass
+
+"""Moving frequency of lectures of courseId"""
+def movingFrequency(courseId):
+    pass
+
+"""Function to count coeficient for tabu tenure (number of conflicting courses / total number of courses)"""
+def coeficientTabuTenure():
+    pass
+
+
+
+
