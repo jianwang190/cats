31c1a852d197c0afdd53da62237078124fcbc764
havk <none>
2013-11-12 22:30:31 +0100

    Zapomnialem ostatiod dodac pliki

---
+++ cats/pso/evaluationFunction.py
@@ -0,0 +1,95 @@
+class EvaluationFunction(object):
+    """Store evaluation functions"""
+    def __init__(self, data, factory):
+        self.data = data
+        self.factory = factory
+
+    def evaluate(self, timetable):
+        """Return sum of penalties"""
+
+        #print "------------------------"
+
+        penalty = self.countRoomCapacityPenalty(timetable)
+        penalty += self.countMinimumWorkingDaysPenalty(timetable)
+        penalty += self.countCurriculumCompactnessPenalty(timetable)
+        penalty += self.countRoomStabilityPenalty(timetable)
+
+        #print "------------------------"
+        return penalty
+
+    def countRoomCapacityPenalty(self, timetable):
+        """Return sum of penalties for room capacity"""
+        penalty = 0
+        for day in range(len(timetable.periods)):
+            for period in range(len(timetable.periods[day])):
+                for room in self.data.rooms:
+                    course = timetable.periods[day][period][room.id]
+                    if course != None:
+                        if room.capacity < course.studentsNum:
+                            penalty += course.studentsNum - room.capacity
+
+        #print penalty
+        return penalty
+
+
+    def countMinimumWorkingDaysPenalty(self, timetable):
+        """Return sum of penalties for minimum  working days"""
+        penalty = 0
+
+        for course in self.data.courses:
+            days = set()
+            for slot in timetable.courses[course.id]:
+                day, _, _ = self.factory.unzip(slot)
+                days.add(day)
+
+            if len(days) < course.minWorkingDays:
+                penalty += (course.minWorkingDays - len(days)) * 5
+
+        #print penalty
+        return penalty
+
+    def countCurriculumCompactnessPenalty(self, timetable):
+        """Return sum of penalties for curriculum compactness"""
+        penalty = 0
+        for curriculum in self.data.curricula:
+            for day in timetable.periods:
+                penaltyDay =0
+                existOne = False
+                for period in day:
+                    wasInPrevousPeriod = False
+                    for room in period:
+                        course = period[room]
+                        if course != None:
+                            if course.id in curriculum.members:
+                                if existOne and not wasInPrevousPeriod:
+                                    penaltyDay += 2
+                                existOne = True
+                                wasInPrevousPeriod = True
+                if penaltyDay > 0:
+                    penalty += penaltyDay + 2
+
+
+        #print penalty
+        return penalty
+
+    def countRoomStabilityPenalty(self, timetable):
+        """Return sum of penalties for room stability"""
+        penalty = 0
+
+        for course in self.data.courses:
+            rooms = set()
+            for slot in timetable.courses[course.id]:
+                _, _, room = self.factory.unzip(slot)
+                rooms.add(room.id)
+
+            if len(rooms) > 1:
+                penalty += len(rooms) - 1
+
+        #print penalty
+        return penalty
+
+
+
+
+
+
---
+++ cats/pso/timetable.py
@@ -0,0 +1,72 @@
+import random
+from copy import deepcopy
+
+class Timetable(object):
+    """Store timetable"""
+
+    def __init__(self, data):
+        rooms = data.rooms
+        daysNum = data.daysNum
+        periodsPerDay = data.periodsPerDay
+        courses = data.courses
+
+        idList = [room.id for room in rooms]
+        noneList = [None for i in range(len(rooms))]
+        roomsDict = dict(zip(idList, noneList))
+        self.periods = [[deepcopy(roomsDict) for j in range(periodsPerDay)] for i in range(daysNum)]
+
+        idList = [course.id for course in courses]
+        tmpList = [set() for i in range(len(courses))]
+        self.courses = dict(zip(idList, tmpList))
+
+        self.penalty = None
+
+
+
+class TimetableFactory(object):
+    """Create timetables"""
+
+    def __init__(self, data):
+        self.data = data
+
+    def getEmptyTimetable(self):
+        """Return empty timetable"""
+
+        return Timetable(self.data)
+
+    def getRandomTimetable(self):
+        """Return random timetable"""
+
+        timetable = self.getEmptyTimetable()
+
+        slots = range(self.data.daysNum * self.data.periodsPerDay * len(self.data.rooms))
+        random.shuffle(slots)
+
+        for course in self.data.courses:
+            for lecture in range(course.lectureNum):
+                slot = slots.pop()
+                day, period, room = self.unzip(slot)
+                timetable.periods[day][period][room.id] = course
+                timetable.courses[course.id].add(slot)
+
+        return timetable
+
+    def getRandomSlot(self):
+        """Return random slot"""
+
+        slot = random.randint(0, self.data.daysNum * self.data.periodsPerDay * len(self.data.rooms) - 1)
+
+        return slot
+
+    def unzip(self, slot):
+        """Unzip day, period and room from slot"""
+
+        day = slot / (self.data.periodsPerDay * len(self.data.rooms))
+        slot -= day * (self.data.periodsPerDay * len(self.data.rooms))
+        period = slot / len(self.data.rooms)
+        slot -= period * len(self.data.rooms)
+        roomNr = slot
+        room = self.data.rooms[roomNr]
+
+        return day, period, room
+
