41ba4e8394196bf7a6d6b776388dfc97395b3248
tomecki <tomek.dziopa@gmail.com>
2013-12-12 13:07:52 +0100

    Tabu dla systemu

---
+++ runTabu.py
@@ -0,0 +1,27 @@
+from cats.adaptiveTabuSearch.tabuSearch import AdaptiveTabuSearch
+
+__author__ = 'tomek'
+import time
+import sys
+
+
+from cats.readers.competitionReader import CompetitionReader
+
+
+fileName = sys.argv[1]
+timeLimit = float(sys.argv[2])
+
+
+c = CompetitionReader()
+data = c.read(fileName)
+
+a = AdaptiveTabuSearch(data, 2400)
+bestSolution = a.run()
+
+for slot in bestSolution.getTimeTable():
+    for lecture in bestSolution.getTimeTable()[slot]:
+        tuple = (slot/data.periodsPerDay, slot % data.periodsPerDay)
+        print lecture[0] + ' ' + lecture[1] + ' ' + str(tuple[0])+ ' ' + \
+            str(tuple[1])
+
+
