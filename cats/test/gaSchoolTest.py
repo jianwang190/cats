9bbfb4c6c32ffba9f43f97cb0cf50ef09697b860
filip <filczaj@gmail.com>
2013-12-13 02:14:06 +0100

    gaschooltest

--- cats/test/gaSchoolTest.py
+++ cats/test/gaSchoolTest.py
@@ -1,12 +1,12 @@
 __author__ = 'filip'
-from cats.ga.GAschool import GeneticAlgorithmSchool
+from cats.ga.geneticAlgorithm import GeneticAlgorithm
 from cats.readers.competitionReader import CompetitionDictReader
 
 class GATest(object):
     def Run(self):
         self.c = CompetitionDictReader()
         self.data = self.c.readInstance(22)
-        self.ga = GeneticAlgorithmSchool(self.data)
+        self.ga = GeneticAlgorithm(self.data)
 
 if __name__=="__main__":
     t = GATest()
